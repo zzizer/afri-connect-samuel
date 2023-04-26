bb86757a9f1155cbf7cb8346384ed2df6a5e764b
Ssekyanzi-Joel <ssekyanzijoel0@gmail.com>
2023-04-26 14:52:24 +0300

    Email Changed

--- base/settings.py
+++ base/settings.py
@@ -141,8 +141,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'africonnect.uganda301@gmail.com'
-EMAIL_HOST_PASSWORD = 'gavlykkuidcvjjwf' 
+EMAIL_HOST_USER = 'africonnectugltd301@gmail.com'
+EMAIL_HOST_PASSWORD = 'tzfjvagdhcuerqxu' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
